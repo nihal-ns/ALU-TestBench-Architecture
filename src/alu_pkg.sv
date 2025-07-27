7aa08aec7099f0125c8801d70c00490d28d992be
Nihal N S <124987234+nihal-ns@users.noreply.github.com>
2025-07-27 22:29:58 +0530

    Create alu_pkg.sv

---
+++ src/alu_pkg.sv
@@ -0,0 +1,11 @@
+package alu_pkg;
+	  import std::*; // some package issue 
+	`include "transaction.sv"
+	`include "generator.sv"
+	`include "driver.sv"
+	`include "monitor.sv"
+	`include "reference.sv"	
+	`include "scoreboard.sv"
+	`include "environment.sv"
+	`include "test.sv"
+endpackage
