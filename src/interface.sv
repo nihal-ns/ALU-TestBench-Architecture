c2b9849b9429ee58e7df0c35e10356d55b1ddd0c
Nihal N S <124987234+nihal-ns@users.noreply.github.com>
2025-07-28 16:45:50 +0530

    Update interface.sv

--- src/interface.sv
+++ src/interface.sv
@@ -1,56 +1,4 @@
-/* `include "defines.sv" */
 
-/* interface intf(input bit clk, rst); */
-/* 	// input side */
-/* 	logic [`WIDTH-1:0] OPA; */
-/* 	logic [`WIDTH-1:0] OPB; */
-/* 	logic [`CMD_WIDTH:0] CMD; */
-/* 	logic CIN; */
-/* 	logic CE; */
-/* 	logic MODE; */
-/* 	logic [1:0] INP_VALID; */
-	
-/* 	// output side */
-/* 	logic [`WIDTH:0]RES; */
-/* 	logic OFLOW; */
-/* 	logic COUT; */
-/* 	logic E; */
-/* 	logic G; */
-/* 	logic L; */
-/* 	logic ERR; */
-
-/* 	clocking mon_cb@(posedge clk); */
-/* 		 default input #0 output #0; */ 
-/* 		input OFLOW, COUT, E, G, L, ERR; */
-/* 		input RES; */
-/* 	endclocking */
-
-/* 	clocking drv_cb@(posedge clk); */
-/* 		 default input #0 output #0; */ 
-/* 		input rst; */
-/* 		output OPA, OPB; */
-/* 		output CMD; */
-/* 		output CE, CIN, MODE; */
-/* 		output INP_VALID; */
-/* 	endclocking */
-
-/* 	clocking ref_cb@(posedge clk); */
-/* 		// changed input#0 */	
-/* 		default input #1 output #0; */
-/* 		input rst, CE, CIN, MODE, INP_VALID, OPA, OPB; */
-/* 		//output RES, COUT, OFLOW, ERR, E, G, L; */
-/* 	endclocking */
-
-/* 	modport MONITOR(clocking mon_cb); */
-/* 	modport DRIVER(clocking drv_cb); */
-/* 	modport REF(clocking ref_cb); */
-/* endinterface */
-
-
-//////
-//
-//
-// ALU Testbench/interface.sv
 `include "defines.sv"
 
 interface intf(input bit clk, rst);
@@ -62,10 +10,8 @@ interface intf(input bit clk, rst);
 	logic [`WIDTH:0] RES;
 	logic OFLOW, COUT, E, G, L, ERR;
 
-	// ** THIS IS THE FIX **
 	clocking mon_cb@(posedge clk);
 		default input #0 output #0;
-		// The monitor needs to see INP_VALID to know when to sample.
 		input INP_VALID,MODE,CMD,CIN,OPA,OPB;
 		input RES, OFLOW, COUT, E, G, L, ERR;
 	endclocking
